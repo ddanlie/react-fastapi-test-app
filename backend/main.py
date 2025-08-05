7d5781a9a75cf358cefad93d8a20735fb980c38b
Domrachev Danil <danildomrachev111@gmail.com>
2025-08-06 00:20:46 +0200

    fix: configs, main.py

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,24 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
-import os
+from pathlib import Path
 
 app = FastAPI()
 
+# Absolute path to index.html using Path
+BASE_DIR = Path(__file__).resolve().parent
+INDEX_PATH = BASE_DIR.parent / "frontend" / "dist" / "index.html"
+
+
 @app.post("/app/{full_path:path}")
 def serve_react_post(full_path: str, request: Request):
-    return FileResponse("frontend/index.html")
+    return FileResponse(INDEX_PATH)
+
 
 @app.get("/app/{full_path:path}")
 def serve_react_get(full_path: str, request: Request):
-    return FileResponse("frontend/index.html")
+    return FileResponse(INDEX_PATH)
 
 
 @app.get("/api/reversed_str/{s}")
 def get_button_name(s: str):
-    return {"button_name": s[::-1]}
+    return {"result": s[::-1]}
